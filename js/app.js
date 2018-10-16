e931fcab086633017aaf77b9c89cb8985b2c9ae6
JennAh <jennah@Jenns-Air.home>
2018-10-16 11:40:15 -1000

    refactoring functions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 
-apiRequest('http://www.reddit.com/r/oddlysatisfying.json', oddlyDiv);
+apiRequest('http://www.reddit.com/r/oddlysatisfying.json', refactorData);
 
 function apiRequest(url, cb) {
   const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', cb);
+  xhr.addEventListener('load', function () {
+    const redData = JSON.parse(this.responseText);
+    const rawChildren = redData.data.children;
+    rawChildren.map(function (article) {
+      return {
+        title: article.data.title,
+        author: article.data.author,
+        date: decodeDate(article),
+        views: article.data.view_count,
+        read: article.data.selftext
+    }
+    });
+  });
   xhr.open('GET', url);
   xhr.send();
 }
@@ -11,80 +23,73 @@ function apiRequest(url, cb) {
 const containerDiv = document.getElementById('container');
 const artDiv = document.getElementById('articleDiv');
 
-// function oddlyDiv() {
+
+function decodeDate (element) {
+  const decodeDate = new Date(element.data.created_utc * 1000);
+  const date = decodeDate.toLocaleDateString();
+  return date;
+};
+
+//function to refactor data from reddit
+// function refactorData() {
 //   const redData = JSON.parse(this.responseText);
-//   const oddElem = document.getElementById('OddlySatisfying')
-//   oddElem.className = 'oddTitle';
-//   oddElem.innerHTML = redData.data.children[0].data.title;
-//   containerDiv.appendChild(oddElem);
+//   const newData = redData.data.children;
 
-//   const oddUser = document.createElement('p');
-//   oddUser.innerHTML = 'by: ' + redData.data.children[0].data.author;
-//   containerDiv.append(oddUser);
+//   newData.forEach(element => {
 
-//   const decodeDate = new Date(redData.data.children[0].data.created_utc * 1000);
-//   const date = decodeDate.toLocaleDateString();
-//   const oddDate = document.createElement('p');
-//   oddDate.innerHTML = date;
-//   containerDiv.append(oddDate);
+//     console.log(element.data);
+//   });
 
-//   const oddViews = document.createElement('p');
-//   oddViews.innerHTML = 'Views ' + redData.data.children[0].data.view_count;
-//   containerDiv.append(oddViews);
 
-//   const oddExcerpt = document.createElement('p');
-//   oddExcerpt.innerHTML = 'Read more...' + redData.data.children[0].data.selftext;
-//   containerDiv.append(oddExcerpt);
+//   const redTitle = document.createElement('title');
+//   redTitle.className = 'titles';
+//   redData.data.children.forEach(element => {
+//     redTitle.innerHTML = 'title: ' + element.data.title;
+//   });
+//   articleDiv.appendChild(redTitle);
+
+//   const redAuthor = document.createElement('p');
+//   redAuthor.className = 'authors';
+//   redData.data.children.forEach(element => {
+//     redAuthor.innerHTML = 'author: ' + element.data.author;
+//   });
+//   articleDiv.appendChild(redAuthor);
+
+//   const redDate = document.createElement('p');
+//   redDate.className = 'dates';
+//   redData.data.children.forEach(element => {
+//     const decodeDate = new Date(element.data.created_utc * 1000);
+//     const date = decodeDate.toLocaleDateString();
+//     redDate.innerHTML = 'date: ' + date;
+//   });
+//   articleDiv.appendChild(redDate);
+
+//   const redViews = document.createElement('p');
+//   redViews.className = 'views';
+//   redData.data.children.forEach(element => {
+//     redViews.innerHTML = 'view count: ' + element.data.view_count;
+//   });
+//   articleDiv.appendChild(redViews);
+
+
+//   const redExcerpt = document.createElement('p');
+//   redExcerpt.className = 'excerpts';
+//   redData.data.children.forEach(element => {
+//     redExcerpt.innerHTML = 'article: ' + element.data.selftext;
+//   });
+//   articleDiv.appendChild(redExcerpt);
+//   containerDiv.appendChild(articleDiv);
+
 
 //   console.log(redData.data.children);
-//   console.log(oddViews);
+// }
+
+
+// function createElems(item, itemClass) {
+//   item.className = itemClass;
 
 // }
 
-//refactoring object
+// function buildCard () {
 
-function oddlyDiv() {
-  const redData = JSON.parse(this.responseText);
-  //const title = redData.data.children[0].data.title;
-  const redTitle = document.createElement('title');
-  redTitle.className = 'titles';
-  redData.data.children.forEach(element => {
-    redTitle.innerHTML = 'title: ' + element.data.title;
-  });
-  articleDiv.appendChild(redTitle);
-
-  const redAuthor = document.createElement('p');
-  redAuthor.className = 'authors';
-  redData.data.children.forEach(element => {
-    redAuthor.innerHTML = 'author: ' + element.data.author;
-  });
-  articleDiv.appendChild(redAuthor);
-
-  const redDate = document.createElement('p');
-  redDate.className = 'dates';
-  redData.data.children.forEach(element => {
-    const decodeDate = new Date(element.data.created_utc * 1000);
-    const date = decodeDate.toLocaleDateString();
-    redDate.innerHTML = 'date: ' + date;
-  });
-  articleDiv.appendChild(redDate);
-
-  const redViews = document.createElement('p');
-  redViews.className = 'views';
-  redData.data.children.forEach(element => {
-    redViews.innerHTML = 'view count: ' + element.data.view_count;
-  });
-  articleDiv.appendChild(redViews);
-
-
-  const redExcerpt = document.createElement('p');
-  redExcerpt.className = 'excerpts';
-  redData.data.children.forEach(element => {
-    redExcerpt.innerHTML = 'article: ' + element.data.selftext;
-  });
-  articleDiv.appendChild(redExcerpt);
-  containerDiv.appendChild(articleDiv);
-
-
-  console.log(redExcerpt);
-}
+// };
